4498bec38161fc5ba6759c621f27d40d2cbc24c3
Waimog <yerko.anton.roj@hotmail.com>
2019-02-28 18:50:49 -0300

    refactoring component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Header from "./header";
 import Footer from "./Footer";
 
-const Layout = ({ children, title, subTitle }) => {
-  return (
-    <div>
-      <Header title={title} subTitle={subTitle} />
-      <section className="section">{children}</section>
-      <Footer />
-    </div>
-  );
-};
+const Layout = ({ children, title, subTitle }) => (
+  <div>
+    <Header title={title} subTitle={subTitle} />
+    <section className="section">{children}</section>
+    <Footer />
+  </div>
+);
 
 export default Layout;
